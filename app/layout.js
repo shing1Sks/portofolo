a8c910a6d9f3d80cb0993f3bc9e48d9bc337bf14
shing1Sks <shreyashks02@gmail.com>
2025-02-18 22:09:14 +0530

    favicon setup

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* <link rel="icon" href="./favicon.png" sizes="any" /> */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
